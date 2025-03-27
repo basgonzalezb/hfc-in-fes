7ce2e59c379487dda3c504fec3c2f30f406aab59
Myxo123 <basgonzalez@udec.cl>
2025-03-27 11:57:52 -0300

    added obtained results, the fep.py code, and some gnuplot codes we won't use

---
+++ cosmo/routines.py
@@ -0,0 +1,124 @@
+from __future__ import division, print_function, absolute_import
+import numpy as np
+
+def ReadCOSMOthermEnergies(filename):
+    data = {}
+    compound = ''
+    temperature = None
+    start_reading = False
+    with open(filename, 'r') as file:
+        for line in file:
+            if 'Property' in line:
+                start_reading = False
+                if line.split(':')[1].split()[2] == 'molecule/conformer':
+                    continue
+            elif 'Settings' in line:
+                temperature = float(line.split('=')[1].split()[0])
+            elif 'Units' in line:
+                E_units = line.split(':')[1].split()[2]
+                if E_units == 'kJ/mol':
+                    Econversion = 1
+                elif p_units == 'kcal/mol':
+                    Econversion = 1/4.184
+            elif 'Nr' in line:
+                start_reading = True
+                continue
+            if start_reading and line.strip():    
+                compound = line.split()[1]
+                hint, hmf, hhb, hvdw, hglt = [float(line.split()[i])*Econversion for i in range(5,10)]
+                data[(temperature, compound)] = [hint, hmf, hhb, hvdw, hglt]
+    return data
+
+def ReadCOSMOthermSolvation(filename):
+    data = {}
+    compound = ''
+    temperature = None
+    start_reading = False
+    with open(filename, 'r') as file:
+        for line in file:
+            if 'Property' in line:
+                start_reading = False
+            elif 'Settings' in line:
+                temperature = float(line.split('=')[1].split()[0])
+            elif 'Units' in line:
+                E_units = line.split(':')[1].split()[2]
+                KH_units = line.split(';')[2].split()[3]
+                if KH_units == 'bar':
+                    KH_conversion = .1
+                elif KH_units == 'MPa':
+                    KH_conversion = 1.
+                if E_units == 'kJ/mol':
+                    Econversion = 1.
+                elif E_units == 'kcal/mol':
+                    Econversion = 1./4.184
+            elif 'Nr' in line:
+                start_reading = True
+                continue
+            if start_reading and line.strip():    
+                compound, KH, lngamma, Gsolv = line.split()[1], line.split()[2], line.split()[3], line.split()[4]
+                if KH != 'NA':
+                    KH = float(KH)*KH_conversion
+                    gamma = np.exp(float(lngamma))
+                    Gsolv = float(Gsolv)*Econversion
+                else:
+                    KH, gamma, Gsolv = None, None, None
+                data[(temperature, compound)] = [KH, gamma, Gsolv]
+    return data
+
+def ReadCOSMOthermGamma(filename):
+    data = {}
+    x1 = None
+    temperature = None
+    x1_vec, gamma_vec = [], []
+    start_reading = False
+    with open(filename, 'r') as file:
+        for line in file:
+            if 'Property' in line:
+                start_reading = False
+            elif 'Settings' in line:
+                temperature = float(line.split('=')[1].split()[0])
+                x1 = float(line.split('=')[2].split()[0])
+            elif 'Nr' in line:
+                start_reading = True
+                continue
+            if start_reading and line.strip():    
+                lngamma = line.split()[3]
+                if lngamma != 'NA':
+                    gamma = np.exp(float(lngamma))
+                else:
+                    gamma = None
+                if gamma is not None:
+                    x1_vec.append(x1), gamma_vec.append(gamma)
+    data[temperature] = [x1_vec, gamma_vec]
+    return data
+
+def ReadCOSMOthermIsotherm(filename):
+    data = {}
+    compound = ''
+    temperature = None
+    start_reading = False
+    with open(filename, 'r') as file:
+        for line in file:
+            if 'Property' in line:
+                start_reading = False
+                if line.split(':')[1].split()[0] == 'LLE':
+                    continue
+                else:
+                    x1, y1, ptot = [], [], []
+            elif 'Compounds' in line:
+                compound = line.split(':')[1].split()[0]
+            elif 'Settings' in line:
+                temperature = float(line.split('=')[1].split()[0])
+            elif 'Units' in line:
+                p_units = line.split(';')[1].split()[2]
+                if p_units == 'mbar':
+                    pconversion = 1e-4
+                elif p_units == 'kPa':
+                    pconversion = 1e-3
+            elif 'x1' in line:
+                start_reading = True
+                continue
+            if start_reading and line.strip():
+                x1.append(float(line.split()[0])), y1.append(float(line.split()[-2])), ptot.append(float(line.split()[4])*pconversion)
+                data[(temperature, compound)] = [x1, y1, ptot]
+    return data
---
+++ md_analysis/fep.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from alchemlyb.workflows import ABFE
+
+os.system('mkdir -p results')
+system = sys.argv[1]
+out = 'results/'+system+'/'
+dir = '../md_files/fep/'+system+'/'
+os.system('mkdir -p results/{}'.format(system))
+workflow = ABFE(software='GROMACS', dir=dir, prefix='fep_', suffix='xvg', T=303.15)
+workflow.update_units('kJ/mol')
+workflow.read(n_jobs=-1)                                        # read the data
+workflow.preprocess(n_jobs=-1, skiptime=30000, uncorr='dhdl', threshold=50)    # decorrelate the data
+workflow.estimate(estimators=("MBAR", "BAR", "TI"))             # run the estimator
+summary = workflow.generate_result()                            # retrieve the result
+workflow.plot_overlap_matrix(overlap=out+'overlap.pdf')         # plot the overlap matrix
+workflow.plot_ti_dhdl(dhdl_TI=out+'dhdl_TI_.pdf')               # plot the dHdl for TI
+workflow.plot_dF_state(dF_state=out+'dF_state_.pdf')            # plot the dF states
+workflow.check_convergence(10, dF_t=out+'dF_t_.pdf')            # convergence analysis
+summary.to_excel(out+'summary.xlsx')
